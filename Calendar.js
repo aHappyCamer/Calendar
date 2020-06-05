4fc3017f69e4f2b729790bb317cb9747db4c70ec
Joey <camph@sheridancollege.ca>
2020-06-05 18:20:59 -0400

    Initial commit

---
+++ Calendar.js
@@ -0,0 +1,91 @@
+class Calendar {
+
+    constructor(){
+        this.time = new Date('May 19, 2020 23:15:30');
+        // this.time = new Date(Date.now());
+        this.year = this.time.getFullYear();
+        this.month = this.time.getMonth() + 1;
+        this.date = this.time.getDate();
+        this.day = this.time.getDay() + 1;
+    }
+
+    getMonthString() {
+        switch(this.month){
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+        }
+    }
+
+    getDayString() {
+        switch(this.day){
+            case 1: return "Sunday";
+            case 2: return "Monday";
+            case 3: return "Tuesday";
+            case 4: return "Wednesday";
+            case 5: return "Thursday";
+            case 6: return "Friday";
+            case 7: return "Saturday";
+        }
+    }
+
+    getTodaysDay(){
+        let a = new Date();
+        return a;
+    }
+
+    getDaysOfMonth(){
+        let date = new Date(this.year, this.month, 0)
+        return date.getDate();
+    }
+
+    getNextMonth(){
+        return new Date(this.year, this.month+1, 1).getMonth();
+        return date.getDate();
+    }
+
+    getFirstDayOfCurrentMonth(){
+        return new Date(this.year, this.month-1, 1).getDay();
+    }
+
+    getFirstDayOfNextMonth(){
+        return new Date(this.year, this.month, 1).getDay() + 1;
+    }
+
+    getLastDayOfCurrentMonth(){
+         return new Date(this.year, this.month, 0).getDay() + 1; // 1 day before June 1 => last day of may
+    }
+
+    getLastDaysOfPreviousMonth(){
+         return new Date(this.year, this.month-1, 1).getDay();
+    }
+
+    getFirstDateOfNextMonth(){
+        return new Date(this.year, this.month, 1);
+    }
+
+    getLastDateOfPreviousMonth(){
+        return new Date(this.year, this.month-1, 0);
+    }
+
+    // show number of days in the previous month
+    getDaysOfPreviousMonth(){
+        return new Date(this.year, this.month-1, 0).getDate();
+    }
+
+    getLastDayOfPreviousMonth(){
+        return new Date(this.year, this.month-2, 1).getDay() + 1;
+    }
+
+
+
+}
---
+++ main.js
@@ -0,0 +1,93 @@
+// document.addEventListener("DOMContentLoaded", function() { 
+//         let calendar = new Calendar();
+//         console.log("Year: " + calendar.year);
+//         console.log("Month: " + calendar.month);
+//         console.log("Date: " + calendar.date);
+//         console.log("Day: " + calendar.day);
+//         console.log("Month String: " + calendar.getMonthString());
+//         console.log("Day String: " + calendar.getDayString());
+//         console.log("Number of Days: " + calendar.getDaysOfMonth());
+//         console.log("First Day Of Month: " + calendar.getFirstDayOfMonth());
+
+//         //debug
+//         console.log(calendar.year);
+//         console.log(calendar.month);
+//         console.log(calendar.day);
+
+//         console.log(calendar.getMonthString()); // May
+//         console.log(calendar.getDayString()); // Tuesday
+//         console.log(calendar.getDaysOfMonth()); // 31
+//         console.log(calendar.getFirstDayOfMonth()); // 6 (friday)
+//     });
+
+let calendar = new Calendar();
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.addEventListener("DOMContentLoaded", function updateMonthYear() { 
+    let displayYear = document.getElementById("currentMonthYear");
+    displayYear.innerHTML = calendar.getMonthString() + " " + calendar.year;
+
+});
+
+document.addEventListener("DOMContentLoaded", function fillInAllDates() { 
+    let list = document.getElementById("gridDaysOfWeek");
+
+    let includePreviousMonthDays = null;
+    let includeNextMonthDays = 7 - calendar.getLastDayOfCurrentMonth();
+    
+    // display days that are in the previous month
+    if (calendar.getFirstDayOfCurrentMonth() >= 1){
+        includePreviousMonthDays = (calendar.getDaysOfPreviousMonth() +1) - (calendar.getFirstDayOfCurrentMonth());
+        for(j = includePreviousMonthDays; j <= calendar.getDaysOfPreviousMonth() ; j++){
+        list.innerHTML += '<div class="gridDaysOfWeekItem">' + j + '</div>';
+        }
+    }
+
+        // list the days for the current month
+    for(i = 1; i <= calendar.getDaysOfMonth(); i++){
+        if(i == 1){
+            list.innerHTML += '<div class="gridDaysOfWeekItem">' + calendar.getMonthString() + i + '</div>';
+        }
+        else if (i == calendar.getTodaysDay()){
+            list.innerHTML += '<div id="currentDay">' + i + '</div>';
+        }
+        else{
+            list.innerHTML += '<div class="gridDaysOfWeekItem">' + i + '</div>'; 
+        }
+    }
+
+    // display the dates for next month
+    
+    if (calendar.getLastDayOfCurrentMonth() <= 7){
+        for (n = 1; n <= includeNextMonthDays; n++){
+            if (includeNextMonthDays == 1) {
+                var d = new Date();
+                list.innerHTML += '<div class="gridDaysOfWeekItem">' + months[d.getMonth()] + n + '</div>';
+            }
+            else{
+                list.innerHTML += '<div class="gridDaysOfWeekItem">' + n + '</div>';
+            }
+        }
+    }
+
+
+    
+    // console.log(calendar.getDaysOfMonth());
+    // console.log(calendar.getLastDayOfCurrentMonth());
+    // console.log(calendar.getDaysOfPreviousMonth()); //31
+    // console.log(calendar.getLastDateOfPreviousMonth());
+    // console.log(calendar.getFirstDayOfNextMonth());
+    // console.log(calendar.getFirstDayOfCurrentMonth());
+    // console.log(calendar.getLastDayOfPreviousMonth());
+    // console.log(includePreviousMonthDays); //30
+    // console.log(calendar.getFirstDayOfNextMonth());
+    console.log(calendar.getNextMonth());
+});
+
+
+
+
+// highlightToday(){
+
+// }
